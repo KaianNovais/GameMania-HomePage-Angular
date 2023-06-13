116518d4d407636ca72f95c53e9937c1556004c1
Kaian Novais <55420281+KaianNovais@users.noreply.github.com>
2023-06-13 16:33:10 -0300

    Add files via upload
    Adicionando todos arquivos do projeto.

---
+++ app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ApresentacaoComponent } from './apresentacao/apresentacao.component';
+import { CardsComponent } from './cards/cards.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    ApresentacaoComponent,
+    CardsComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
